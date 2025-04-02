417c1aee5cf91bad3ddfdbfabeac540fcec99cd3
saccharin01 <chousik01@gmail.com>
2025-04-02 15:29:57 +0900

    new | PostDto.java
    데이터베이스 전송에 사용될 간단한 dto 정의 및 lombokc 을 사용한 getter, setter 매핑

---
+++ src/main/java/com/springboot/springtest/dto/PostDto.java
@@ -0,0 +1,13 @@
+package com.springboot.springtest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class PostDto {
+    private String name;
+    private String hobby;
+
+}
